207488361b1e6aa2bcdcaa26930a04321b7ff49a
olawale-o <omoogunolawale@gmail.com>
2024-09-18 09:35:58 +0100

    Extend tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,10 +11,24 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        black: {
+          warm: "#3A3845",
+          brown: "#826F66",
+        },
+        neutral: {
+          "200": "#F7F6F5",
+          "300": "#E5E5E5",
+          "400": "#CAC9CF",
+          "500": "#A6A6A8",
+          "600": "#807F86",
+          "700": "#595667",
+          "800": "#3A3845",
+        },
       },
       fontFamily: {
         inter: "var(--font-inter)",
         garammond: "var(--font-garammond)",
+        g: "var(--font-g)",
       },
     },
   },
